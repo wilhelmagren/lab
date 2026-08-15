99e072c981f8a7169048c55cbc452759d8135b50
wilhelmagren <wilhelmagren98@gmail.com>
2026-08-16 00:44:44 +0200

    lexical analysis done, with cstyle block comments

--- books/crafting-interpreters/src/main.rs
+++ books/crafting-interpreters/src/main.rs
@@ -63,19 +63,47 @@ enum TokenType {
     Eof,
 }
 
+impl TokenType {
+    pub fn from_identifier(chars: &[char]) -> Option<Self> {
+        let ident = chars.iter().cloned().collect::<String>();
+        let tt = match ident.as_str() {
+            "and" => TokenType::And,
+            "class" => TokenType::Class,
+            "else" => TokenType::Else,
+            "false" => TokenType::False,
+            "for" => TokenType::For,
+            "fun" => TokenType::Fun,
+            "if" => TokenType::If,
+            "nil" => TokenType::Nil,
+            "or" => TokenType::Or,
+            "print" => TokenType::Print,
+            "return" => TokenType::Return,
+            "super" => TokenType::Super,
+            "this" => TokenType::This,
+            "true" => TokenType::True,
+            "var" => TokenType::Var,
+            "while" => TokenType::While,
+            _ => {
+                return None;
+            }
+        };
+        Some(tt)
+    }
+}
+
 #[derive(Clone, Debug)]
 enum Literal<'a> {
-    String(&'a str),
+    String(&'a [char]),
     Number(f64),
-    Bool(bool),
-    Nil,
 }
 
 impl<'a> std::fmt::Display for Literal<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Nil => write!(f, "Nil"),
-            s => write!(f, "{}", s),
+            Self::String(s) => {
+                write!(f, "{}", s.iter().copied().collect::<String>())
+            }
+            Self::Number(n) => write!(f, "{}", n),
         }
     }
 }
@@ -85,6 +113,7 @@ struct Token<'a> {
     type_: TokenType,
     lexeme: &'a [char],
     literal: Option<Literal<'a>>,
+    indices: (usize, usize),
     line: usize,
 }
 
@@ -93,12 +122,14 @@ impl<'a> Token<'a> {
         type_: TokenType,
         lexeme: &'a [char],
         literal: Option<Literal<'a>>,
+        indices: (usize, usize),
         line: usize,
     ) -> Self {
         Self {
             type_,
             lexeme,
             literal,
+            indices,
             line,
         }
     }
@@ -109,18 +140,23 @@ impl<'a> std::fmt::Display for Token<'a> {
         if let Some(lit) = &self.literal {
             write!(
                 f,
-                "{:?} {} {}",
+                "Token({:?}, lexeme='{}', literal='{}', line={}, indices=[{},{}))",
                 self.type_,
                 &String::from_iter(self.lexeme),
                 lit,
+                self.line,
+                self.indices.0,
+                self.indices.1,
             )
         } else {
             write!(
                 f,
-                "{:?} {} {}",
+                "Token({:?}, lexeme='{}', literal=None, line={}, indices=[{},{}))",
                 self.type_,
                 &String::from_iter(self.lexeme),
-                "None",
+                self.line,
+                self.indices.0,
+                self.indices.1,
             )
         }
     }
@@ -146,12 +182,14 @@ impl<'a> Scanner<'a> {
     }
 
     fn add_token(&mut self, type_: TokenType, literal: Option<Literal<'a>>) {
-        self.buffer.push(Token::new(
+        let t = Token::new(
             type_,
             &self.source[self.l..self.r],
             literal,
+            (self.l, self.r),
             self.line,
-        ));
+        );
+        self.buffer.push(t);
     }
 
     fn try_match(&mut self, expected: char) -> bool {
@@ -166,6 +204,19 @@ impl<'a> Scanner<'a> {
         return self.r >= self.source.len();
     }
 
+    fn is_digit(&self, c: char) -> bool {
+        // this works cus chars are ASCII numbers ordered
+        c >= '0' && c <= '9'
+    }
+
+    fn is_alpha(&self, c: char) -> bool {
+        (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
+    }
+
+    fn is_alpha_numeric(&self, c: char) -> bool {
+        self.is_alpha(c) || self.is_digit(c)
+    }
+
     fn peek(&self) -> char {
         if self.is_at_end() {
             return '\0';
@@ -173,9 +224,17 @@ impl<'a> Scanner<'a> {
         self.source[self.r]
     }
 
+    fn peek_next(&self) -> char {
+        let next = self.r + 1;
+        if next >= self.source.len() {
+            return '\0';
+        }
+        self.source[next]
+    }
+
     // the rules that determine how a particular language groups characters into
     // lexemes are called its lexical grammar.
-    pub fn scan_tokens(&mut self) -> impl Iterator<Item = Token<'a>> {
+    pub fn scan_tokens(&mut self) -> Option<impl Iterator<Item = Token<'a>>> {
         while !self.is_at_end() {
             let c = self.source[self.r];
 
@@ -221,15 +280,107 @@ impl<'a> Scanner<'a> {
                     };
                     self.add_token(tt, None);
                 }
+                // Challenges 4: Add support for C-style `/* ... */` block comments.
                 '/' => {
-                    if self.try_match('/') {
-                        // A comment goes until the end of the line.
+                    //where is self.r?
+                    let np = self.peek();
+
+                    // A normal comment goes until the end of the line.
+                    if np == '/' {
+                        self.r += 1;
                         while self.peek() != '\n' && !self.is_at_end() {
                             self.r += 1;
+                        }
+                    } else if np == '*' {
+                        self.r += 1;
+
+                        // a block comment goes until the next time we see */
+                        let mut peeked = self.peek();
+                        while !self.is_at_end() {
+                            if peeked == '*' && self.peek_next() == '/' {
+                                // skip those chars
+                                self.r += 2;
+                                break;
+                            }
+
+                            // need to do this before peeking again
+                            // otherwise we miss counting a \n
+                            if peeked == '\n' {
+                                self.line += 1;
+                            }
+
+                            self.r += 1;
+                            peeked = self.peek();
                         }
                     } else {
                         self.add_token(TokenType::Slash, None);
                     }
+                }
+                // noop
+                ' ' | '\r' | '\t' => {}
+                '\n' => {
+                    self.line += 1;
+                }
+                '"' => {
+                    let mut peeked = self.peek();
+                    while peeked != '"' && !self.is_at_end() {
+                        if peeked == '\n' {
+                            self.line += 1;
+                        }
+                        self.r += 1;
+                        peeked = self.peek()
+                    }
+
+                    if self.is_at_end() {
+                        error(self.line, "Unterminated string.");
+                        return None;
+                    }
+
+                    self.r += 1;
+
+                    self.add_token(
+                        TokenType::String,
+                        Some(Literal::String(&self.source[self.l + 1..self.r - 1])),
+                    );
+                }
+                '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                    while self.is_digit(self.peek()) {
+                        self.r += 1;
+                    }
+
+                    // looking past the decimal point requires a second character of
+                    // lookahead since we don't want to soncume the '.' until we're
+                    // really sure there is a digit after it
+                    if self.peek() == '.' && self.is_digit(self.peek_next()) {
+                        self.r += 1;
+                        while self.is_digit(self.peek()) {
+                            self.r += 1;
+                        }
+                    }
+                    self.add_token(
+                        TokenType::Number,
+                        // this is really ugly but yea, that's we way we have to do it
+                        Some(Literal::Number(
+                            self.source[self.l..self.r]
+                                .iter()
+                                .copied()
+                                .collect::<String>()
+                                .parse::<f64>()
+                                .unwrap(),
+                        )),
+                    );
+                }
+                'A'..='Z' | 'a'..='z' | '_' => {
+                    while self.is_alpha_numeric(self.peek()) {
+                        self.r += 1;
+                    }
+
+                    let tt = match TokenType::from_identifier(&self.source[self.l..self.r]) {
+                        Some(tt) => tt,
+                        None => TokenType::Identifier,
+                    };
+
+                    self.add_token(tt, None);
                 }
                 _ => {
                     error(
@@ -244,9 +395,14 @@ impl<'a> Scanner<'a> {
             }
         }
 
-        self.buffer
-            .push(Token::new(TokenType::Eof, &[], None, self.line));
-        self.buffer.clone().into_iter()
+        self.buffer.push(Token::new(
+            TokenType::Eof,
+            &[],
+            None,
+            (self.l, self.r),
+            self.line,
+        ));
+        Some(self.buffer.clone().into_iter())
     }
 }
 
@@ -262,8 +418,10 @@ fn error(line: usize, msg: &str) {
 fn run(source: String) {
     let charvec = source.chars().collect::<Vec<char>>();
     let mut scanner = Scanner::new(&charvec);
-    for token in scanner.scan_tokens() {
-        println!("{}", token);
+    if let Some(iterator) = scanner.scan_tokens() {
+        for token in iterator {
+            println!("{}", token);
+        }
     }
 }
 
